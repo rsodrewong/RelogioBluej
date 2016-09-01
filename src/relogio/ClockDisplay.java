bbfb397faaa57ee7ac15cb42c34764ac672e92f2
rogerio <rogerio@lenovo-50>
2016-09-01 09:37:07 -0300

    Codigo do relogio Bluej

---
+++ src/relogio/ClockDisplay.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relogio;
+
+/**
+ *
+ * @author rogerio
+ */
+public class ClockDisplay {
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    
+    
+    
+}
